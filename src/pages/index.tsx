6a489713e53933fe686f8763478ac400f29508cd
Jaber Elferkh <elferkhjaber@gmail.com>
2022-04-23 02:25:56 +0000

    Deleting some unused files 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import PageLayout from '@components/PageLayout';
 import Hero from '@containers/Home/Hero';
 import TrustedCompanies from '@containers/Home/TrustedCompanies';
 import Head from 'next/head';
-import ImageTesting from '@containers/Home/ImageTesting';
 
 const Home: NextPage = () => {
   return (
@@ -18,24 +17,12 @@ const Home: NextPage = () => {
         <meta name='author' content='Jaber' />
         <meta name='description' content='The best NFT Marketplace' />
 
-        {/* <meta property='og:title' content={metaTitle} />
-        <meta property='og:description' content={metaDescription} />
-        <meta property='og:image' content={ogImage} />
-        <meta property='og:image:height' content='630' />
-        <meta property='og:image:width' content='1200' />
-        <meta name='twitter:title' content={metaTitle} />
-        <meta name='twitter:image' content={ogImage} />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:description' content={metaDescription} /> */}
-
         <link rel='shortcut icon' href='/nft-favicon.png' type='image/x-icon' />
-        {/* <link rel='icon' href='/favicon.png' /> */}
       </Head>
 
       <PageLayout>
         <Hero />
         <TrustedCompanies />
-        {/* <ImageTesting /> */}
       </PageLayout>
     </>
   );
